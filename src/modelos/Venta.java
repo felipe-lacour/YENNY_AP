4a479a9ada78d636891a0296e46576c4c766112b
felipe-lacour <felipelacourhughes@gmail.com>
2024-05-28 16:09:51 -0300

    Perchance
    Inicio trabajo venta.java

--- src/modelos/Venta.java
+++ src/modelos/Venta.java
@@ -1,16 +1,23 @@
 package modelos;
 import java.time.LocalDate;
 
+import controladores.PromocionControlador;
+import controladores.VentaControlador;
+
 public class Venta {
 	private int ventaId;
 	private int metodoPagoId;
 	private LocalDate fecha;
+	private VentaControlador controlVenta;
+	private PromocionControlador controlPromocion;
 	
 	public Venta(int ventaId, int metodoPagoId, LocalDate fecha) {
 		super();
 		this.ventaId = ventaId;
 		this.metodoPagoId = metodoPagoId;
 		this.fecha = fecha;
+		this.controlVenta = new VentaControlador();
+		this.controlPromocion = new PromocionControlador();
 	}
 
 	public int getVentaId() {
@@ -38,4 +45,6 @@ public class Venta {
 	}
 	
 	
+	
 }
+
