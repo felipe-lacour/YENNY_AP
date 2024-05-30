075edbc7deea209d2ee1fb23e04eecd1cad0a540
Brandan2005 <priscila.brandan@davinci.edu.ar>
2024-05-30 10:43:13 -0300

    test
    ..

--- src/test/LibreriaTest.java
+++ src/test/LibreriaTest.java
@@ -1,8 +1,9 @@
 package test;
 import static org.junit.Assert.*;
-
 import java.util.List;
 import javax.swing.JOptionPane;
+
+import org.junit.Before;
 import org.junit.Test;
 import controladores.DatabaseConnection;
 import controladores.LibroControlador;
@@ -13,60 +14,50 @@ import interfaces.LibroRepositorio;
 
 public class LibreriaTest {
 	private LibroControlador libroControlador;
-	 private GerenteSucursal gerenteSucursal;
+	private GerenteSucursal gerenteSucursal;
+	private Libro libro;
 	 
-	 @Test
-	 public void testSolicitarLibroPorNombreExistente() {
-	        Libro nuevoLibro = new Libro(0, "Libro Existente", null, 1, 1);
-	        libroControlador.addBook(nuevoLibro);
-	        solicitarLibroPorNombre(libroControlador, "Libro Existente", 10);
-	        List<Libro> libros = libroControlador.getAllBooks();
-	        boolean libroEncontrado = libros.stream().anyMatch(libro -> libro.getTitulo().equals("Libro Existente"));
-	        assertTrue(libroEncontrado);
-	    }
+	@Before
+    public void setUp() {
+        libroControlador = new LibroControlador();
+        gerenteSucursal = new GerenteSucursal(1, "GerenteSucursal", 1, 1, "password", "pedro");
+        libro = new Libro(1, "Harry Potter y la piedra filosofal", null, 1, 1);
+        libroControlador.addBook(libro);
+    }
+	@Test
+    public void testObtenerLibrosDisponibles() {
+        List<Libro> libros = libroControlador.getAllBooks();
+        assertTrue("Debe haber libros disponibles.", libros.size() > 0);
+    }
+	@Test
+    public void testSolicitarLibroPorNombreExistente() {
+        solicitarLibroPorNombre(libroControlador, "Harry Potter y la piedra filosofal", 10);
+    }
+	public void solicitarLibroPorNombre(LibroControlador libroControlador, String nombre, int cantidad) {
+        List<Libro> libros = libroControlador.getAllBooks();
+        boolean libroEncontrado = false;
+
+        for (Libro libro : libros) {
+            if (libro.getTitulo().equalsIgnoreCase(nombre)) {
+                if (cantidad > 0) {
+                    System.out.println("Libro solicitado exitosamente. Cantidad: " + cantidad);
+                } else {
+                    System.out.println("Cantidad no válida. La cantidad debe ser mayor que cero.");
+                }
+                libroEncontrado = true;
+                break;
+            }
+        }
+
+        if (!libroEncontrado) {
+            fail("El libro no está disponible.");
+        }
+    }
 	 @Test
 	    public void testSolicitarLibroSinCantidad() {
-	        Libro nuevoLibro = new Libro(0, "Libro Sin Cantidad", null, 1, 1);
-	        libroControlador.addBook(nuevoLibro);
-	        solicitarLibroPorNombre(libroControlador, "Libro Sin Cantidad", 0);
-	        List<Libro> libros = libroControlador.getAllBooks();
-	        boolean libroEncontrado = libros.stream().anyMatch(libro -> libro.getTitulo().equals("Libro Sin Cantidad"));
-	        assertTrue(libroEncontrado);
+		 solicitarLibroPorNombre(libroControlador, "Libro de Prueba", 0);
 	    }
-	 @Test
-	 public void solicitarLibroPorNombre(LibroControlador libroControlador, String nombre, int cantidad) {
-	        List<Libro> libros = libroControlador.getAllBooks();
-	        boolean libroEncontrado = false;
-	        for (Libro libro : libros) {
-	            if (libro.getTitulo().equalsIgnoreCase(nombre)) {
-	                JOptionPane.showMessageDialog(null, "Libro solicitado exitosamente.");
-	                libroEncontrado = true;
-	                break;
-	            }
-	        }
-	        if (!libroEncontrado) {
-	            JOptionPane.showMessageDialog(null, "El libro no está disponible.");
-	        }
-	    }
-	 @Test
-	 public void menuSolicitarLibro(LibroControlador libroControlador) {
-	        List<Libro> libros = libroControlador.getAllBooks();
-	        if (libros.isEmpty()) {
-	            JOptionPane.showMessageDialog(null, "No hay libros disponibles.");
-	            return;
-	        }
-
-	        String[] titulosLibros = libros.stream().map(Libro::getTitulo).toArray(String[]::new);
-	        String libroSeleccionado = (String) JOptionPane.showInputDialog(null, "Seleccione un libro:", "Libros disponibles", JOptionPane.QUESTION_MESSAGE, null, titulosLibros, titulosLibros[0]);
-
-	        if (libroSeleccionado != null) {
-	            String cantidadStr = JOptionPane.showInputDialog("Ingrese la cantidad que desea solicitar:");
-	            if (cantidadStr != null) {
-	                int cantidad = Integer.parseInt(cantidadStr);
-	                solicitarLibroPorNombre(libroControlador, libroSeleccionado, cantidad);
-	            }
-	        }
-	    }
+	 
 	 
 	
  
